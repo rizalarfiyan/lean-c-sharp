ca8e2c87e5e9a101a0ad9dcdc6fac20fe67814a2
Muhamad Rizal Arfiyan <rizal.arfiyan.23@gmail.com>
2023-03-08 15:37:15 +0700

    feat: add feature type validation and fix concat string in app_calulator

--- calculator_apps/Program.cs
+++ calculator_apps/Program.cs
@@ -24,16 +24,24 @@ namespace CalculatorApps
             return firstvalue / secondvalue;
         }
 
-        static void Main(string[] args)
+        static void PrintAppName()
         {
-            Console.Title = "Calculator Apps";
-            const int MAX_MENU = 4;
-
             Console.WriteLine("-----------------------------------------------------------");
             Console.WriteLine("--------------------- Calculator Apps ---------------------");
             Console.WriteLine("---------------- @rizalarfiyan - 22.11.5227 ---------------");
             Console.WriteLine("-----------------------------------------------------------");
             Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            const int MAX_MENU = 4;
+
+            main:
+            int chooseMenu, firstvalue, secondvalue;
+            Console.Title = "Calculator Apps";
+            Console.Clear();
+            PrintAppName();
 
             Console.WriteLine(" Menu: ");
             Console.WriteLine(" 1. Sum");
@@ -42,34 +50,63 @@ namespace CalculatorApps
             Console.WriteLine(" 4. Division");
             Console.WriteLine();
 
+            chose_menu:
             Console.Write("Choose your menu: ");
-            int chooseMenu = Convert.ToInt32(Console.ReadLine());
-            if (chooseMenu < 0 || chooseMenu > MAX_MENU)
+            if (!int.TryParse(Console.ReadLine(), out chooseMenu))
             {
-                Console.WriteLine("Please input number between 1-{0}", MAX_MENU);
-                System.Environment.Exit(0);
+                Console.Write("Please input a valid number!\n");
+                goto chose_menu;
             }
 
-            Console.Write("Input fist value: ");
-            int firstvalue = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Input second value: ");
-            int secondvalue = Convert.ToInt32(Console.ReadLine());
+            if (chooseMenu < 0 || chooseMenu > MAX_MENU)
+            {
+                Console.Write("Please input number between 1-{0}\n", MAX_MENU);
+                goto chose_menu;
+            }
+
+            input_first_value:
+            Console.Write("\nInput fist value: ");
+            if (!int.TryParse(Console.ReadLine(), out firstvalue))
+            {
+                Console.Write("Please input a valid number!\n");
+                goto input_first_value;
+            }
+
+            input_second_value:
+            Console.Write("\nInput second value: ");
+            if (!int.TryParse(Console.ReadLine(), out secondvalue))
+            {
+                Console.Write("Please input a valid number!\n");
+                goto input_second_value;
+            }
 
             switch (chooseMenu)
             {
                 case 1:
-                    Console.WriteLine("Result Sum of {0} + {1} = {3}", firstvalue, secondvalue, Sum(firstvalue, secondvalue));
+                    Console.WriteLine("Result Sum of {0} + {1} = {2}", firstvalue, secondvalue, Sum(firstvalue, secondvalue));
                     break;
                 case 2:
-                    Console.WriteLine("Result Subtraction of {0} - {1} = {3}", firstvalue, secondvalue, Subtraction(firstvalue, secondvalue));
+                    Console.WriteLine("Result Subtraction of {0} - {1} = {2}", firstvalue, secondvalue, Subtraction(firstvalue, secondvalue));
                     break;
                 case 3:
-                    Console.WriteLine("Result Multiply of {0} * {1} = {3}", firstvalue, secondvalue, Multiply(firstvalue, secondvalue));
+                    Console.WriteLine("Result Multiply of {0} * {1} = {2}", firstvalue, secondvalue, Multiply(firstvalue, secondvalue));
                     break;
                 case 4:
-                    Console.WriteLine("Result Division of {0} / {1} = {3}", firstvalue, secondvalue, Division(firstvalue, secondvalue));
+                    Console.WriteLine("Result Division of {0} / {1} = {2}", firstvalue, secondvalue, Division(firstvalue, secondvalue));
                     break;
             }
+
+            Console.Write("\nPress y or Y to continue: ");
+            char confirm = Console.ReadKey().KeyChar;
+            if (confirm == 'y' || confirm == 'Y')
+            {
+                goto main;
+            }
+
+            Console.Write("\n\n");
+            Console.WriteLine("-----------------------------------------------------------");
+            Console.WriteLine("------------------------ Thank You ------------------------");
+            PrintAppName();
         }
     }
 }
