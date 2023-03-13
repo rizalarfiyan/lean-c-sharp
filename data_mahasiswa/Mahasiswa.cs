6c537a77ba83e2d6eaaae656f129e008753d0f3b
Muhamad Rizal Arfiyan <rizal.arfiyan.23@gmail.com>
2023-03-13 14:27:54 +0700

    featL add data mahasiswa, learn class and object

---
+++ data_mahasiswa/Mahasiswa.cs
@@ -0,0 +1,31 @@
+﻿namespace DataMahasiswa
+{
+    internal class Mahasiswa
+    {
+
+        // properties
+        public string Nama { get; set; }
+        public int NIM { get; set; }
+        public string Jurusan { get; set; }
+        public int Angkatan { get; set; }
+
+        // constructor
+        public Mahasiswa(string nama, int nim, string jurusan, int angkatan)
+        {
+            Nama = nama;
+            NIM = nim;
+            Jurusan = jurusan;
+            Angkatan = angkatan;
+        }
+
+        // method
+        public void PrintInfo()
+        {
+            Console.WriteLine("Nama\t\t: " + Nama);
+            Console.WriteLine("NIM\t\t: " + NIM);
+            Console.WriteLine("Jurusan\t\t: " + Jurusan);
+            Console.WriteLine("Angkatan\t: " + Angkatan);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ data_mahasiswa/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace DataMahasiswa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Mahasiswa mhs1 = new Mahasiswa("John Doe", 22104114, "Teknik Informatika", 2022);
+            Mahasiswa mhs2 = new Mahasiswa("Jane Doe", 220711241, "Sistem Informasi", 2022);
+            Mahasiswa mhs3 = new Mahasiswa("Muhamad Rizal Arfiyan", 22115227, "S1 Informatika", 2022);
+
+            mhs1.PrintInfo();
+            mhs2.PrintInfo();
+            mhs3.PrintInfo();
+
+            Console.ReadKey();
+        }
+    }
+}
