4153e5c1fcb72247a3264c1988c87791804e7414
Muhamad Rizal Arfiyan <rizal.arfiyan.23@gmail.com>
2023-06-05 15:09:45 +0700

    fix: comment name

--- polymorpism_printer/Printer Polimorphism/Epson.cs
+++ polymorpism_printer/Printer Polimorphism/Epson.cs
@@ -1,12 +1,10 @@
 ﻿class Epson: PrinterWindows
 {
-    // Default function Show()
     public override void Show()
     {
         Console.WriteLine("Epson display dimension: 10*11");
     }
 
-    // Default function Print()
     public override void Print()
     {
         Console.WriteLine("Epson printing....");
--- polymorpism_printer/Printer Polimorphism/PrinterWindows.cs
+++ polymorpism_printer/Printer Polimorphism/PrinterWindows.cs
@@ -1,10 +1,12 @@
 ﻿class PrinterWindows
 {
+    // Default function Show()
     public virtual void Show()
     {
         Console.WriteLine("Printer Windows display dimension: ");
     }
 
+    // Default function Print()
     public virtual void Print()
     {
         Console.WriteLine("Printer Windows printing....");
