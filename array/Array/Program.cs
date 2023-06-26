bd5ecb11955d42f262acd3827449e9a07384a3de
Muhamad Rizal Arfiyan <rizal.arfiyan.23@gmail.com>
2023-06-26 09:01:22 +0700

    feat: add array code

---
+++ array/Array/Program.cs
@@ -0,0 +1,32 @@
+﻿class Program
+{
+    static void Main(String[] args)
+    {
+        string[] arrNama = { "erick", "wely", "sony", "joni" };
+        Console.WriteLine(arrNama[2].ToString() + "\n");
+
+        for (int i = 0; i < arrNama.Length; i++)
+        {
+            Console.WriteLine(arrNama[i]);
+        }
+        Console.WriteLine();
+
+        foreach (string nama in arrNama)
+        {
+            Console.WriteLine(nama);
+        }
+        Console.WriteLine();
+
+        int[] arrBil = new int[3];
+        arrBil[0] = 12;
+        arrBil[1] = 14;
+        arrBil[2] = 24;
+
+        foreach(int bil in arrBil)
+        {
+            Console.WriteLine(bil.ToString());
+        }
+
+        Console.ReadKey();
+    }
+}
